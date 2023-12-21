224ce356cffa5fa1b51a077a2d1387dc433f5a49
KaiShinozakiConefrey <100449691+KaiShinozakiConefrey@users.noreply.github.com>
2023-12-21 00:19:44 -0500

    Init

---
+++ Dungeon/game.py
@@ -0,0 +1,236 @@
+import pygame
+import random
+
+pygame.init()
+
+# 9 - player
+# 8 - wall
+# 7 - unmapped
+# 5 - footsteps
+# 4 - door
+
+class GameInformation:
+    def __init__(self, moves, gameState):
+        self.moves = moves
+        self.gameState = gameState
+
+class Game:
+    MAPLENGTH = 16
+    CELLSIZE = 32
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GAME_FONT = pygame.font.SysFont("comicsans", 50)
+
+    doorx = 1
+    doory = 14
+
+    def lookFor(self, x, y, type):
+        return (self.map[x][y] == type)
+
+    def generate_maze(self):
+        
+        random.seed(456)
+
+        #Populate the maze with breakable walls        
+        self.map = [[7 for x in range(self.MAPLENGTH)] for y in range(self.MAPLENGTH)]
+
+        #Make the boundaries
+        for x in range(self.MAPLENGTH):
+            for y in range(self.MAPLENGTH):
+                if(x == 0 or x == self.MAPLENGTH-1):
+                    self.map[x][y] = 8
+                if(y == 0 or y == self.MAPLENGTH-1):
+                    self.map[x][y] = 8
+
+        posX = 1
+        posY = 14
+        holes = 0
+        dirs = [[1, 0],[0, 1], [-1, 0],[0, -1]]
+        tunneling = True
+
+        backtrackfailsafe = 0
+        backtrackfailsafeMax = 25
+
+        self.doorx = 1
+        self.doory = 14
+
+        self.map[posX][posY] = 5
+        
+        #Tunnel randomly through the maze, creating walls and backtracking if necessary
+        while tunneling:
+            rots = 0
+            dir = random.randint(0, 3)
+            searching = True
+            while searching:
+                theDir = dirs[dir]
+                if(self.lookFor(posX + theDir[0], posY + theDir[1], 7)):
+                    
+                    if(self.lookFor(posX + theDir[1], posY + theDir[0], 7)):
+                        self.map[posX + theDir[1]][posY + theDir[0]] = 8
+                    if(self.lookFor(posX - theDir[1], posY - theDir[0], 7)):
+                        self.map[posX - theDir[1]][posY - theDir[0]] = 8
+                    
+                    posX += theDir[0]
+                    posY += theDir[1]
+
+                    self.map[posX][posY] = dir
+
+                    searching = False
+                    holes += 1
+                    backtrackfailsafe = 0
+                    if(posX > 10 or posY < 6):
+                        self.doorx = posX
+                        self.doory = posY
+
+                    if(holes > 120):
+                        tunneling = False
+                
+                dir = (dir + 1) % 4
+                rots += 1
+
+                #Nowhere left to go, so backtrack one step
+                if(rots > 4):
+                    backtrackfailsafe += 1
+                    if(backtrackfailsafe < backtrackfailsafeMax):
+                        searching = False
+                        returnDir = self.map[posX][posY]
+                        if(returnDir > 3):
+                            tunneling = False
+                        else:  
+                            posX -= dirs[returnDir][0]
+                            posY -= dirs[returnDir][1]
+                    else:
+                        tunneling = False
+                        searching = False
+        
+        #Make sure the door is placed properly
+        addDoor = (self.doorx == 1 and self.doory == 14)
+        if(not addDoor):
+           self.map[self.doorx][self.doory] = 4
+
+        while(addDoor):
+            self.doorx = random.randint(1, 14)
+            self.doory = random.randint(1, 14)
+            if(self.map[self.doorx][self.doory] < 4):
+                self.map[self.doorx][self.doory] = 4
+                addDoor = False
+
+        self.map[1][14] = 0
+        self.map[2][14] = 0
+        self.map[3][14] = 0
+        self.map[1][13] = 0
+        self.map[2][13] = 0
+        self.map[3][13] = 0
+        self.map[1][12] = 0
+        self.map[2][12] = 0
+        self.map[3][12] = 0
+
+        self.map[2][13] = 9
+
+        
+
+    def __init__(self, window, window_width, window_height):
+        self.window_width = window_width
+        self.window_height = window_height
+        self.window = window
+        self.gameState = 0
+
+        self.generate_maze()
+
+        self.playerX = 2
+        self.playerY = 13
+
+        self.map[self.playerX][self.playerY] = 9
+
+        self.moves = 0
+        self.newMoves = 0
+
+        self.PLAYERIMAGE = pygame.image.load("Dungeon/Theseus.png").convert()
+        self.PLAYERIMAGE = pygame.transform.scale(self.PLAYERIMAGE, (32, 32))
+
+        self.WALLIMAGE = pygame.image.load("Dungeon/Wall.png").convert()
+        self.WALLIMAGE = pygame.transform.scale(self.WALLIMAGE, (32, 32))
+
+        self.DOORIMAGE = pygame.image.load("Dungeon/Door.png").convert()
+        self.DOORIMAGE = pygame.transform.scale(self.DOORIMAGE, (32, 32))
+
+        self.FOOTSTEPIMAGE = pygame.image.load("Dungeon/Footsteps.png").convert()
+        self.FOOTSTEPIMAGE = pygame.transform.scale(self.FOOTSTEPIMAGE, (32, 32))
+
+
+    def draw(self):
+        if(self.gameState == 0):
+            self.window.fill(self.BLACK)
+            for x in range(self.MAPLENGTH):
+                for y in range(self.MAPLENGTH):
+                    if(self.map[x][y] == 9):
+                        self.window.blit(self.PLAYERIMAGE, (x* self.CELLSIZE, y* self.CELLSIZE) )
+                    elif(self.map[x][y] == 8):
+                        self.window.blit(self.WALLIMAGE, (x* self.CELLSIZE, y* self.CELLSIZE) )
+                    elif(self.map[x][y] == 7):
+                        self.window.blit(self.WALLIMAGE, (x* self.CELLSIZE, y* self.CELLSIZE) )
+                    elif(self.map[x][y] == 5):
+                        self.window.blit(self.FOOTSTEPIMAGE, (x* self.CELLSIZE, y* self.CELLSIZE) )
+                    elif(self.map[x][y] == 4):
+                        self.window.blit(self.DOORIMAGE, (x* self.CELLSIZE, y* self.CELLSIZE) )
+            
+            moves_score = self.GAME_FONT.render("MOVES: " + f"{self.newMoves}", 1, self.RED)
+            self.window.blit(moves_score, (self.MAPLENGTH * self.CELLSIZE / 2, self.MAPLENGTH * self.CELLSIZE))
+        if(self.gameState == 1):
+            self.window.fill(self.BLACK)
+            moves_score = self.GAME_FONT.render("YOU WIN", 1, self.RED)
+            self.window.blit(moves_score, (0, self.MAPLENGTH * self.CELLSIZE / 2))
+
+            moves_score = self.GAME_FONT.render("Press Enter to retry", 1, self.RED)
+            self.window.blit(moves_score, (0, self.MAPLENGTH * self.CELLSIZE / 2 + 48))
+
+    def move_player(self, x, y):
+        self.moves += 1
+        if(self.map[self.playerX + x][self.playerY + y] < 6):
+            self.map[self.playerX][self.playerY] = 5
+            self.playerX += x
+            self.playerY += y
+            if(self.map[self.playerX][self.playerY] < 5):
+                self.newMoves += 1
+
+            if(self.map[self.playerX][self.playerY] == 4):
+                self.gameState = 1
+
+            self.map[self.playerX][self.playerY] = 9       
+        game_info = GameInformation(
+            self.moves, self.gameState)
+
+        return game_info
+
+    def get_inputs(self):
+        wall_left1 = self.map[self.playerX - 1][self.playerY] > 6
+        wall_right1 = self.map[self.playerX + 1][self.playerY] > 6
+        wall_down1 = self.map[self.playerX][self.playerY + 1] > 6
+        wall_up1 = self.map[self.playerX][self.playerY - 1] > 6
+        
+        wall_left2 = self.playerX == 1 or self.map[self.playerX - 2][self.playerY] > 6
+        wall_right2 = self.playerX == 14 or self.map[self.playerX + 2][self.playerY] > 6
+        wall_down2 = self.playerY == 14 or self.map[self.playerX][self.playerY + 2] > 6
+        wall_up2 = self.playerY == 1 or self.map[self.playerX][self.playerY - 2] > 6
+
+        #print("wall_left1 ", wall_left1)
+        #print("wall_left2 ", wall_left2)
+        #print("wall_right1 ", wall_right1)
+        #print("wall_right2 ", wall_right2)
+        #print("wall_up1 ", wall_up1)
+        #print("wall_up2 ", wall_up2)
+        #print("wall_down1 ", wall_down1)
+        #print("wall_down2 ", wall_down2)
+
+        #return (wall_left1, wall_right1, wall_down1, wall_up1, self.playerX, self.playerY, self.doorx, self.doory)
+        return (wall_left1, wall_right1, wall_down1, wall_up1, wall_left2, wall_right2, wall_down2, wall_up2, self.playerX, self.playerY, self.doorx, self.doory)
+
+    def restart(self):
+        self.playerX = 2
+        self.playerY = 13
+        self.moves = 0
+        self.gameState = 0
+        self.newMoves = 0
+        self.generate_maze()
+    
+        
---
+++ main.py
@@ -0,0 +1,169 @@
+import pygame
+import os
+from Dungeon import Game
+import neat
+import pickle
+
+class MazeGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+
+    def test_game(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(10)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            
+            keys = pygame.key.get_pressed()
+            if(self.game.gameState == 0):
+                if keys[pygame.K_UP]:
+                    self.game.move_player(0, -1)
+                if keys[pygame.K_DOWN]:
+                    self.game.move_player(0, 1)
+                if keys[pygame.K_RIGHT]:
+                    self.game.move_player(1, 0)
+                if keys[pygame.K_LEFT]:
+                    self.game.move_player(-1, 0)
+
+            if keys[pygame.K_RETURN] and self.game.gameState == 1:
+                self.game.restart()
+
+            self.game.get_inputs()
+            self.game.draw()
+            pygame.display.update()
+    
+    def train_ai(self, genome, config):
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        run = True
+
+        totalMoves = 0
+        totalNewMoves = 0
+        wins = 0
+
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+            
+            output = net.activate( self.game.get_inputs() )
+            decision = output.index(max(output))
+            if decision == 0:
+                self.game.move_player(1, 0)
+            if decision == 1:
+                self.game.move_player(0, 1)
+            if decision == 2:
+                self.game.move_player(-1, 0)
+            if decision == 3:
+                self.game.move_player(0, -1)
+            
+            self.game.draw()
+            pygame.display.update()
+
+            if(self.game.gameState == 1):
+                totalMoves += self.game.moves
+                totalNewMoves += self.game.newMoves
+                wins += 1
+                self.game.restart()
+
+            if(self.game.moves > 200 or wins > 6):
+                totalMoves += self.game.moves
+                totalNewMoves += self.game.newMoves
+                self.calculate_fitness(genome, totalMoves, wins, totalNewMoves)
+                break
+    
+    def test_ai(self, genome, config):
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        run = True
+
+        totalMoves = 0
+        totalNewMoves = 0
+        wins = 0
+
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(10)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+            
+            output = net.activate( self.game.get_inputs() )
+            decision = output.index(max(output))
+            if decision == 0:
+                self.game.move_player(1, 0)
+            if decision == 1:
+                self.game.move_player(0, 1)
+            if decision == 2:
+                self.game.move_player(-1, 0)
+            if decision == 3:
+                self.game.move_player(0, -1)
+            
+            self.game.draw()
+            pygame.display.update()
+
+            if(self.game.gameState == 1):
+                totalMoves += self.game.moves
+                totalNewMoves += self.game.newMoves
+                wins += 1
+                self.game.restart()
+
+    def calculate_fitness(self, genome, totalMoves, wins, newMoves):
+        closenessScore = (16 - abs(self.game.playerX - self.game.doorx) + 16 - abs(self.game.playerY - self.game.doory)) * 4
+        genome.fitness = wins * 50 + newMoves + closenessScore
+
+
+def test_game():
+    width, height = 640, 640
+    window = pygame.display.set_mode((width, height))
+    game = MazeGame(window, width, height)
+    game.test_game()
+
+def eval_genomes(genomes, config):
+    width, height = 640, 640
+    window = pygame.display.set_mode((width, height))
+
+    for genome_id, genome in genomes:
+            game = MazeGame(window, width, height)
+            game.train_ai(genome, config)
+
+
+def run_neat(config):
+    p = neat.Population(config)
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-85')
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+
+    winner = p.run(eval_genomes, 150)
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+    
+def test_ai(config):
+    width, height = 640, 640
+    window = pygame.display.set_mode((width, height))
+
+    with open("best.pickle", "rb") as f:
+        winner = pickle.load(f)
+
+    game = MazeGame(window, width, height)
+    game.test_ai(winner, config)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    
+    #test_game()
+    #run_neat(config)
+    test_ai(config)
